b59c5c62540a88e5f7fd8f03bb20d6ed8877ea86
jamesbopape <jamesbopape@gmail.com>
2024-03-12 05:42:19 -0700

    FGG

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing size');
-} else {
-  const x = Number(process.argv[2]);
+exports.esrever = function (list) {
+  let len = list.length - 1;
   let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
     i++;
+    len--;
   }
-}
+  return list;
+};
