12c37e4ff18df6c186a325a2bc929f3c01996965
jamesbopape <jamesbopape@gmail.com>
2024-03-12 05:29:44 -0700

    ddd

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
 class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-            return {}; // Return an empty object if conditions are met
-        }
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
